a7c0b8e3f92a2561b51a311fa7451c15752c2169
liewrichmond <richmond.liew.97@gmail.com>
2021-02-09 14:06:23 -0500

    testing

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
+import socket
+
 def main():
     print("Your code goes here!")
 
-    # Uncomment this to pass the first stage
-    #
-    import socket
     s = socket.create_server(("localhost", 6379), reuse_port=True)
-    s.accept() # wait for client
+    connection, addr= s.accept() # wait for client
+
+    connection.rcv(4096)
+
+
+
+
+
 
 
 if __name__ == "__main__":
