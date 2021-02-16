8751d2fd992126c64e48a5474c75006f325d84e2
liewrichmond <richmond.liew.97@gmail.com>
2021-02-16 15:09:21 -0500

    added a loop to cotinually read data

--- app/main.py
+++ app/main.py
@@ -11,18 +11,20 @@ def ping(arg):
     return data
 
 def main():
-    #print("Starting Server...")
-    s = socket.create_server(("localhost", 6379), reuse_port=True)
+    print("Starting Server...")
+    s = socket.create_server(('localhost', 6379), reuse_port=True)
     connection, addr= s.accept() # wait for client
-
-    data = connection.recv(4096)
-    parser = Parser()
-    cmdArray = parser.parseArray(data)
-    cmds = parser.parseCommands(cmdArray)
-    pingOutput = ping(cmds[0][1])
-    connection.sendall(pingOutput)
-    # Client commands normally array of bulk strings
-    # Sample Input :   b'*1\r\n$4\r\nping\r\n'
+    while True:
+        data = connection.recv(1024)
+        if data == b'':
+            continue
+        parser = Parser()
+        cmdArray = parser.parseArray(data)
+        cmds = parser.parseCommands(cmdArray)
+        pingOutput = ping(cmds[0][1])
+        connection.sendall(pingOutput)
+        # Client commands normally array of bulk strings
+        # Sample Input :   b'*1\r\n$4\r\nping\r\n'
 
 if __name__ == "__main__":
     main()
