1a36755e56a5461091f873c649d47df9c1b81e5e
liewrichmond <richmond.liew.97@gmail.com>
2021-02-19 13:01:03 -0500

    concurrent clients working. Implementing timeouts

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+import asyncio
 from .parser import Parser
 from .encoder import Encoder
 
@@ -10,23 +11,62 @@ def ping(arg):
         data = encoder.encodeSimpleString(arg)
     return data
 
-def main():
-    print("Starting Server...")
-    s = socket.create_server(('localhost', 6379), reuse_port=True)
-    connection, addr= s.accept() # wait for client
+async def listenForCommands(connection):
+    print("listening for commands")
+    connection.settimeout(20)
+    parser = Parser()
     while True:
-        connection.settimeout(5)
         data = connection.recv(1024)
+
         if data == b'':
-            print(1)
             continue
-        parser = Parser()
+
         cmdArray = parser.parseArray(data)
         cmds = parser.parseCommands(cmdArray)
         pingOutput = ping(cmds[0][1])
         connection.sendall(pingOutput)
-        # Client commands normally array of bulk strings
-        # Sample Input :   b'*1\r\n$4\r\nping\r\n'
+
+class RedisServer():
+    def __init__(self):
+        self.connections = []
+
+    async def listenForClient(self):
+        print("listening for client")
+        s = socket.create_server(('localhost', 6379), reuse_port = True)
+        connection, addr = s.accept()
+        return connection
+
+    async def listenForCommands(self, connection):
+        print("listeningForCommands")
+        connection.settimeout(20)
+        parser = Parser()
+        while True:
+            data = connection.recv(1024)
+
+            if data == b'':
+                await asyncio.sleep(0.1)
+                continue
+
+            cmdArray = parser.parseArray(data)
+            cmds = parser.parseCommands(cmdArray)
+            pingOutput = ping(cmds[0][1])
+            connection.sendall(pingOutput)
+
+    async def acceptNewClient(self):
+        connection = await self.listenForClient()
+        asyncio.create_task(self.listenForCommands(connection))
+
+async def main():
+    print("Starting Server...")
+    server = RedisServer()
+    loop = asyncio.get_event_loop()
+    listenTask = asyncio.create_task(server.acceptNewClient())
+    while True:
+        await asyncio.sleep(0.1)
+        if(listenTask.done()):
+            listenTask = asyncio.create_task(server.acceptNewClient())
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    #asyncio.run(main())
